1a853f9ac819a9478160aca45d5c2046457c752d
Madhusudan Ravi <madhuravius@protonmail.com>
2025-10-09 20:27:00 -0400

    chore: broke up debug apps into isolated tools

---
+++ cmd/debug/tools/file_system/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/madhuravius/brains/internal/tools/file_system"
+
+	"github.com/pterm/pterm"
+)
+
+func main() {
+	fs, err := file_system.NewFileSystemConfig()
+	if err != nil {
+		pterm.Fatal.Printfln("file_system.NewFileSystemConfig: %v", err)
+	}
+
+	fsData, err := fs.SetContextFromGlob("README.md")
+	if err != nil {
+		pterm.Fatal.Printfln("file_system.SetContextFromGlob: %v", err)
+	}
+	pterm.Info.Printfln("data from glob gather: %s", fsData)
+}
